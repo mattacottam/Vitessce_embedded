c7ce086b3a3cda0c682a82626fe8e10ac645652a
mattacottam <mattacottam@DESKTOP-L2MJGAM>
2023-05-04 13:46:27 -0500

    test again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Vitessce } from 'vitessce';
 import { myViewConfig } from './my-view-config';
-import 'vitessce/dist/es/production/static/css/index.css';
+import './index.css';
 
 export default function App() {
   return (
